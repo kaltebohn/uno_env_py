ab02d2757f626d20c6348121ce2e4e48a97a9590
araki-tnlab <coldtoufu.dev@gmail.com>
2023-04-12 18:16:41 +0900

    change: 複数のエージェントがいる場合を考え、環境クラスでエージェントの番号を指定できるように変更。

--- uno_env.py
+++ uno_env.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from os import urandom
 
 from game.state import State
 
@@ -8,14 +9,13 @@ class UnoEnv:
     強化学習における環境。
     Stateクラスを保持する。
     エージェントとのやり取りを行う。
-    プレイヤ番号は0で固定。
     """
 
     def __init__(self):
         """ゲームの初期化。
         """
-        self.player_number = 0
-        self.agent_state = State()
+        self.state = State(urandom(16)) # 128ビットの乱数を渡す。
+        self.current_agent = self.state.current_player
 
     def legal_actions(self) -> list:
         """現在の状態で合法な行動集合を返す。
@@ -23,9 +23,7 @@ class UnoEnv:
         Returns:
             list: 合法な行動のリスト。
         """
-        if self.player_number != self.agent_state.current_player:
-            return []
-        return self.agent_state.legal_actions()
+        return self.state.legal_actions()
 
     def next_state(self, state: State, action) -> State:
         """与えられた状態で与えられた行動を取った際の次状態を返す。
@@ -39,7 +37,7 @@ class UnoEnv:
         """
         return state.next(action)
 
-    def reward(self, state: State, action, next_state: State) -> int:
+    def reward(self, state: State, action, next_state: State) -> list[int]:
         """報酬関数。
 
         Args:
@@ -48,14 +46,19 @@ class UnoEnv:
             next_state (State): 次状態。
 
         Returns:
-            int: 次状態に遷移したとき得られた報酬・
+            int: 次状態に遷移したとき得られた報酬のリスト。
         """
-        return next_state.player_scores()[self.player_number]
+        return next_state.player_scores()
 
     def reset(self):
-        """ゲームの状態を初期化。
+        """ゲームの状態を初期化して最初の観測を返す。
+
+        Returns:
+            int, dict: 最初のエージェントの添え字、最初のエージェントの観測。
         """
-        self.agent_state = State()
+        self.state = State()
+        self.current_agent = self.state.current_player
+        return self.current_agent, self.state.player_observation(self.current_agent)
 
     def step(self, action):
         """与えられた行動を適用して状態遷移を行い、エージェントに結果を与える。
@@ -64,12 +67,13 @@ class UnoEnv:
             action (_type_): 取る行動。
 
         Returns:
-            _type_: [観測、報酬、ゲームが終了したか？]のリスト。UNOは不完全情報ゲームなので、エージェントは状態ではなく観測を与えられる。
+            _type_: [現在のエージェントの添え字、観測、報酬、ゲームが終了したか？]のリスト。UNOは不完全情報ゲームなので、エージェントは状態ではなく観測を与えられる。
         """
-        state = deepcopy(self.agent_state)
+        state = deepcopy(self.state)
         next_state = self.next_state(state, action)
         reward = self.reward(state, action, next_state)
         done = next_state.is_finished()
 
-        self.agent_state = next_state
-        return next_state.player_observation(self.player_number), reward, done
+        self.state = next_state
+        self.current_agent = self.state.current_player
+        return self.current_agent, next_state.player_observation(self.current_agent), reward, done
