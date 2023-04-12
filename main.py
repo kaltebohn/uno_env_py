10c57461874a3b366903c15c14b60b78f5e96792
araki-tnlab <coldtoufu.dev@gmail.com>
2023-04-12 18:16:59 +0900

    add: main関数追加。

---
+++ main.py
@@ -0,0 +1,20 @@
+from game.consts import NUM_OF_PLAYERS
+from random_agent import RandomAgent
+from uno_env import UnoEnv
+
+env = UnoEnv()
+agents = [RandomAgent() for _ in range(NUM_OF_PLAYERS)]
+
+current_agent_idx, observation = env.reset()
+
+while True:
+    action = agents[current_agent_idx].get_action(observation)
+    print(action)
+    current_agent_idx, next_observation, reward, done = env.step(action)
+
+    if done:
+        break
+
+    observation = next_observation
+
+print(env.state.player_scores())
---
+++ random_agent.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class RandomAgent:
+    """ランダムな行動を取るだけのエージェント。
+    """
+    def get_action(self, observation: dict):
+        """行動を選ぶ。
+
+        Args:
+            observation (dict): 環境から与えられる観測。
+
+        Returns:
+            _type_: 選択した行動。
+        """
+        # ここで引っかかったらロジックがおかしい。
+        assert len(observation["legal_actions"]) > 0
+
+        return np.random.choice(observation["legal_actions"])
